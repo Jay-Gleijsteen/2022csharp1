324a3d3a7849de8e69f5ad8b072803b439052d8d
jay gleijsteen <36629@ma-web.nl>
2024-02-27 10:32:08 +0100

    push 2

---
+++ CSharpOpdrachten/HelloWorld/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http.Headers;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("H");
+            Thread.Sleep(100);
+            Console.WriteLine("E");
+            Thread.Sleep(100);
+            Console.WriteLine("L");
+            Thread.Sleep(100);
+            Console.WriteLine("L");
+            Thread.Sleep(100);
+            Console.WriteLine("O");
+            Thread.Sleep(100);
+            Console.WriteLine(" ");
+            Thread.Sleep(1000);
+            Console.WriteLine("W");
+            Thread.Sleep(100);
+            Console.WriteLine("O");
+            Thread.Sleep(100);
+            Console.WriteLine("R");
+            Thread.Sleep(100);
+            Console.WriteLine("L");
+            Thread.Sleep(100);
+            Console.WriteLine("D");
+            Thread.Sleep(100);
+        }
+    }
+}
---
+++ CSharpOpdrachten/Time/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Time
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i != 0; i++)
+            {
+                Console.WriteLine(DateTime.Now.ToString("H:mm:ss"));
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+        }  
+    }
+}
